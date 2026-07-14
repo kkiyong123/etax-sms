2a80398b5c89505436348c7acf86169092be497b
kkiyong123 <98229313+kkiyong123@users.noreply.github.com>
2026-07-14 22:29:11 +0900

    Add src/main/java/lk/gov/ird/etax/sms/EtaxSmsApplication.java

---
+++ src/main/java/lk/gov/ird/etax/sms/EtaxSmsApplication.java
@@ -0,0 +1,11 @@
+package lk.gov.ird.etax.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtaxSmsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EtaxSmsApplication.class, args);
+    }
+}
